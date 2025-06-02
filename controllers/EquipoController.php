146a7204c1407841e1884f0c507e3481f67515ed
JuanFolleti <23.daw.juancanomediel@fundacionmontessori.com>
2025-06-02 19:35:19 +0200

    Actualizo proyecto con últimos cambios

--- controllers/EquipoController.php
+++ controllers/EquipoController.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($nombre && $escudo && $equipacion && $estadio) {
         if (Equipo::crear($nombre, $escudo, $equipacion, $entrenador_id, $estadio)) {
-            header('Location:index.php');
+            header('Location:../index.php');
             exit();
         } else {
             $error = "Error al crear el equipo.";
